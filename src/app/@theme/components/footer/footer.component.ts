6fd7892a50a60ad468cc2c2ccdfa80389a738cb6
waqar.khan <waqar.khan@techloyce.com>
2020-06-24 17:32:40 +0500

    init admin panel

---
+++ src/app/@theme/components/footer/footer.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-footer',
+  styleUrls: ['./footer.component.scss'],
+  template: `
+    <span class="created-by">
+      Copyrights © 2020 <b><a href="https://subscriptionflow.com" target="_blank" rel="nofollow" class="text-white">SubscriptionFlow</a></b>
+    </span>
+    <div class="socials">
+      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
+      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
+      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
+    </div>
+  `,
+})
+export class FooterComponent {
+}
---
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import {
+  NbActionsModule,
+  NbButtonModule,
+  NbCardModule,
+  NbTabsetModule,
+  NbUserModule,
+  NbRadioModule,
+  NbSelectModule,
+  NbListModule,
+  NbIconModule,
+} from '@nebular/theme';
+import { NgxEchartsModule } from 'ngx-echarts';
+
+import { ThemeModule } from '../../@theme/theme.module';
+import { DashboardComponent } from './dashboard.component';
+import { TemperatureComponent } from './temperature/temperature.component';
+import { ElectricityComponent } from './electricity/electricity.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [
+    FormsModule,
+    ThemeModule,
+    NbCardModule,
+    NbUserModule,
+    NbButtonModule,
+    NbTabsetModule,
+    NbActionsModule,
+    NbRadioModule,
+    NbSelectModule,
+    NbListModule,
+    NbIconModule,
+    NbButtonModule,
+    NgxEchartsModule,
+  ],
+  declarations: [
+    DashboardComponent,
+    TemperatureComponent,
+    ElectricityComponent,
+  ],
+})
+export class DashboardModule { }
